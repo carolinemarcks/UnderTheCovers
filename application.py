3675efb44a9e036f135f4c51314cbedb6f428a2c
Caroline Marcks <caroline.marcks@tufts.edu>
2013-11-09 13:51:10 -0500

    add searching for I want you back

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import render_template
 
 from pyechonest import config
 from pyechonest import artist
+from pyechonest import song
 
 config.ECHO_NEST_API_KEY = "Z5YYKIYYQG3NZEXWN"
 
@@ -10,13 +11,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  weezer_results = artist.search(name="weezer")
-  weezer = weezer_results[0]
-  weezer_blogs = weezer.blogs
-  print 'Blogs about weezer:', [blog.get('url') for blog in weezer_blogs]
   return render_template('index.html')
 
 
+@app.route("/iwub")
+def iwub(title=None):
+  lsd = song.search(title="I want you back",artist="Lake Street Dive", buckets=["id:rdio-US"])
+  print lsd
+  for entry in lsd:
+    print entry.artist_name
+
+  jsf = song.search(title="I want you back",artist="The Jackson 5", buckets=["id:rdio-US"])
+  print jsf
+  for entry in jsf:
+    print entry.artist_name
+
+  jm = song.search(title="I want you back",artist="David Ruffin", buckets=["id:rdio-US"])
+  print jm
+  for entry in jm:
+    print entry.artist_name
+
+
+
+  return render_template('index.html')
 
 if __name__ == "__main__":
+  app.debug = True
   app.run()
+  
