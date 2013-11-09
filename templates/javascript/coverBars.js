68d3c6b9aa9136dc136c09aebad1e4cad845dbf4
AaronTufts <aarontietz@gmail.com>
2013-11-09 16:10:31 -0500

    added basic client

---
+++ templates/javascript/coverBars.js
@@ -0,0 +1,49 @@
+var colors = ["steelblue", "red", "green"];
+
+var covers = [
+		{artist  : "Michael Jackson",
+		 seconds : 240},
+		{artist  : "Bjork",
+		 seconds : 223},
+		{artist  : "R Kelly",
+		 seconds : 250}
+		];
+
+var width = 800,
+    barHeight = 80,
+    textJump = 30,
+    textStart = 30
+    barHeightWithBuffer = 1.1 * barHeight;
+
+function secondsList(a){
+	returnArray = new Array()
+	for(var i = 0; i < covers.length; i++)
+		returnArray.push(a[i].seconds);
+	return returnArray;
+}
+
+var scale = d3.scale.linear()
+    .domain([0, d3.max(secondsList(covers))])
+    .range([0, width]);
+
+function updateChart(){
+	var chart = d3.select(".chart")
+	    .attr("width", width)
+	    .attr("height", barHeightWithBuffer * covers.length );
+
+	var bar = chart.selectAll("g")
+	    .data(covers)
+	    .enter().append("g")
+	    .attr("transform", function(d, i) {return "translate(0," + i * barHeightWithBuffer + ")"; })
+	    .attr("style", function(d, i) {return "fill:" + colors[i % colors.length];});
+
+	bar.append("rect")
+	    .attr("width", function (d) { return scale(d.seconds)})
+	    .attr("height", barHeight - 1);
+
+	bar.append("text")
+	    .attr("x", function(d, i) { return textStart + (textJump * i); })
+	    .attr("y", barHeight / 2)
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d.artist; });
+}
